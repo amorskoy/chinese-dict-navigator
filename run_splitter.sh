2250538d582c102fc2880af2dd28b9a114d3c6de
Andrey Morskoy <morskoy.a@gmail.com>
2023-09-07 15:52:59 +0300

    Fix broken multiline in sentence splitter

--- run_splitter.sh
+++ run_splitter.sh
@@ -2,6 +2,6 @@ while true
 do
   echo "Paste your text and press CTRL+d when you finish. To exit press CTRL+c"
   sentence=$(cat)
-  python3 py/split_sentences.py "$sentence" | grep -vP "^[\n]*$" | xclip
+  python3 py/split_sentences.py "$sentence" | grep -vP "^[\n]*$" | sed "s/ //g" | xclip
   echo "Split copied to clipboard"
 done
